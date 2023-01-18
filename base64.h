807afafac9f9eab13f6a3320bde157dcbaa5bdf0
sh3d0ww01f <31366374+sh3d0ww01f@users.noreply.github.com>
2023-01-18 17:05:49 +0800

    Create base64.h

---
+++ base64.h
@@ -0,0 +1,9 @@
+#ifndef   __base64__
+#define   __base64__
+#include<string>
+#include<string.h>
+#include<vector>
+int gcd(int,int);
+std::string base64encode(std::string);
+std::string base64decode(std::string);
+#endif
